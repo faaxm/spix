8c728ad7aafe6413ef9d405d3435a42b8ec2bb19
Lubos Dolezel <lubos@dolezel.info>
2019-07-17 12:03:16 +0200

    Fix indentation in DragEnd.cpp

--- lib/src/Commands/DragEnd.cpp
+++ lib/src/Commands/DragEnd.cpp
@@ -20,22 +20,22 @@ DragEnd::DragEnd(ItemPath path, std::promise<void> promise)
 
 void DragEnd::execute(CommandEnvironment& env)
 {
-	try {
-		auto item = env.scene().itemAtPath(m_path);
+    try {
+        auto item = env.scene().itemAtPath(m_path);
 
-		if (!item)
+        if (!item)
             throw std::runtime_error("DragEnd: Item not found: " + m_path.string());
 
-		auto size = item->size();
-		Point midPoint(size.width / 2.0, size.height / 2.0);
-		env.scene().events().mouseMove(item.get(), midPoint);
-		env.scene().events().mouseUp(item.get(), midPoint, Events::MouseButtons::left);
+        auto size = item->size();
+        Point midPoint(size.width / 2.0, size.height / 2.0);
+        env.scene().events().mouseMove(item.get(), midPoint);
+        env.scene().events().mouseUp(item.get(), midPoint, Events::MouseButtons::left);
 
-		m_promise.set_value();
-	} catch (const std::runtime_error& e) {
+        m_promise.set_value();
+    } catch (const std::runtime_error& e) {
         env.state().reportError(e.what());
         m_promise.set_exception(std::make_exception_ptr(e));
-	}
+    }
 }
 
 } // namespace cmd
