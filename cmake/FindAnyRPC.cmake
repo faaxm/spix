b7c5800a45153a84e7005751133907a80f92c222
Alexis Placet <aplacet@dxo.com>
2019-08-13 17:41:05 +0200

    Change the way we search AnyRPC

--- cmake/FindAnyRPC.cmake
+++ cmake/FindAnyRPC.cmake
@@ -1,14 +1,40 @@
 include(FindPackageHandleStandardArgs)
 
-find_path(AnyRPC_INCLUDE_DIRS anyrpc/anyrpc.h)
-find_library(AnyRPC_LIBRARIES NAMES anyrpc)
+set(AnyRPC_INCLUDE_DIR_HINT "" CACHE PATH "AnyRPC include dir")
+set(AnyRPC_LIBRARY_DIR_HINT "" CACHE PATH "AnyRPC library dir")
+set(AnyRPC_VERBOSE_FIND ON CACHE BOOL "AnyRPC verbosisty")
 
-find_package_handle_standard_args(AnyRPC DEFAULT_MSG AnyRPC_INCLUDE_DIRS AnyRPC_LIBRARIES )
-mark_as_advanced(AnyRPC_INCLUDE_DIRS AnyRPC_LIBRARIES )
+find_path(AnyRPC_INCLUDE_DIRS
+  anyrpc/anyrpc.h
+  HINTS
+    ${AnyRPC_INCLUDE_DIR_HINT})
+
+if(${AnyRPC_VERBOSE_FIND})
+  message(STATUS "AnyRPC include dir : ${AnyRPC_INCLUDE_DIRS}")
+endif()
+
+find_library(AnyRPC_LIBRARIES 
+  NAMES
+    anyrpc
+  HINTS
+    ${AnyRPC_LIBRARY_DIR_HINT})
+
+if(${AnyRPC_VERBOSE_FIND})
+    message(STATUS "AnyRPC libraries dir : ${AnyRPC_LIBRARIES}")
+endif()
+
+find_package_handle_standard_args(AnyRPC
+  DEFAULT_MSG
+  AnyRPC_INCLUDE_DIRS
+  AnyRPC_LIBRARIES)
+
+mark_as_advanced(AnyRPC_INCLUDE_DIRS
+  AnyRPC_LIBRARIES)
 
 if( ANYRPC_FOUND )
   if( NOT TARGET AnyRPC::anyrpc )
     add_library(AnyRPC::anyrpc UNKNOWN IMPORTED)
+
     set_target_properties(AnyRPC::anyrpc PROPERTIES
       IMPORTED_LINK_INTERFACE_LANGUAGES "CXX"
       IMPORTED_LOCATION ${AnyRPC_LIBRARIES}
