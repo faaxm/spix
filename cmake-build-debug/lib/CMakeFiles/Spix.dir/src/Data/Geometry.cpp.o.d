5d6e5b5d6109c1c2019ccadfc290b1da5396455e
Jan-Ole Schuemann <j.schuemann@kruss.de>
2021-05-31 10:48:11 +0200

    remove clion cmake build directory

--- cmake-build-debug/lib/CMakeFiles/Spix.dir/src/Data/Geometry.cpp.o.d
+++
@@ -1,4 +0,0 @@
-lib/CMakeFiles/Spix.dir/src/Data/Geometry.cpp.o: \
- /home/developer/workspace/spix/lib/src/Data/Geometry.cpp \
- /usr/include/stdc-predef.h \
- /home/developer/workspace/spix/lib/include/Spix/Data/Geometry.h
