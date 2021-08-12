7bf69396e5a5baaca6bea765bcc38847ebbc072f
David Cole <dcole@neocis.com>
2021-08-12 15:56:14 -0400

    Formatting changes for QtEvents.cpp after running ./devtools/run-clang-format.sh

--- lib/src/Scene/Qt/QtEvents.cpp
+++ lib/src/Scene/Qt/QtEvents.cpp
@@ -24,183 +24,183 @@ namespace spix {
 
 namespace {
 
-QQuickWindow* getWindowAndPositionForItem(Item* item, Point relToItemPos, QPointF& windowPos)
-{
-    auto qtitem = dynamic_cast<QtItem*>(item);
-    if (!qtitem)
-        return nullptr;
-    auto window = qtitem->qquickitem()->window();
+QQuickWindow *getWindowAndPositionForItem(Item *item, Point relToItemPos,
+                                          QPointF &windowPos) {
+  auto qtitem = dynamic_cast<QtItem *>(item);
+  if (!qtitem)
+    return nullptr;
+  auto window = qtitem->qquickitem()->window();
 
-    QPointF qtlocalPoint(relToItemPos.x, relToItemPos.y);
-    windowPos = qtitem->qquickitem()->mapToScene(qtlocalPoint);
+  QPointF qtlocalPoint(relToItemPos.x, relToItemPos.y);
+  windowPos = qtitem->qquickitem()->mapToScene(qtlocalPoint);
 
-    return window;
+  return window;
 }
 
-Qt::MouseButton getQtMouseButtonValue(Events::MouseButton button)
-{
-    unsigned qtButton = Qt::MouseButton::NoButton;
+Qt::MouseButton getQtMouseButtonValue(Events::MouseButton button) {
+  unsigned qtButton = Qt::MouseButton::NoButton;
 
-    if (button & Events::MouseButtons::left) {
-        qtButton |= Qt::MouseButton::LeftButton;
-    }
+  if (button & Events::MouseButtons::left) {
+    qtButton |= Qt::MouseButton::LeftButton;
+  }
 
-    if (button & Events::MouseButtons::right) {
-        qtButton |= Qt::MouseButton::RightButton;
-    }
+  if (button & Events::MouseButtons::right) {
+    qtButton |= Qt::MouseButton::RightButton;
+  }
 
-    if (button & Events::MouseButtons::middle) {
-        qtButton |= Qt::MouseButton::MiddleButton;
-    }
+  if (button & Events::MouseButtons::middle) {
+    qtButton |= Qt::MouseButton::MiddleButton;
+  }
 
-    return static_cast<Qt::MouseButton>(qtButton);
+  return static_cast<Qt::MouseButton>(qtButton);
 }
 
-Qt::KeyboardModifiers getQtKeyboardModifiers(QtEvents::KeyModifier mod)
-{
-    Qt::KeyboardModifiers qtmod = Qt::NoModifier;
+Qt::KeyboardModifiers getQtKeyboardModifiers(QtEvents::KeyModifier mod) {
+  Qt::KeyboardModifiers qtmod = Qt::NoModifier;
 
-    if (mod & QtEvents::KeyModifiers::shift) {
-        qtmod = qtmod | Qt::ShiftModifier;
-    }
-    if (mod & QtEvents::KeyModifiers::control) {
-        qtmod = qtmod | Qt::ControlModifier;
-    }
-    if (mod & QtEvents::KeyModifiers::alt) {
-        qtmod = qtmod | Qt::AltModifier;
-    }
-    if (mod & QtEvents::KeyModifiers::meta) {
-        qtmod = qtmod | Qt::MetaModifier;
-    }
+  if (mod & QtEvents::KeyModifiers::shift) {
+    qtmod = qtmod | Qt::ShiftModifier;
+  }
+  if (mod & QtEvents::KeyModifiers::control) {
+    qtmod = qtmod | Qt::ControlModifier;
+  }
+  if (mod & QtEvents::KeyModifiers::alt) {
+    qtmod = qtmod | Qt::AltModifier;
+  }
+  if (mod & QtEvents::KeyModifiers::meta) {
+    qtmod = qtmod | Qt::MetaModifier;
+  }
 
-    return qtmod;
+  return qtmod;
 }
 
-void sendQtKeyEvent(Item* item, bool press, int keyCode, QtEvents::KeyModifier mod)
-{
-    auto qtitem = dynamic_cast<QtItem*>(item);
-    if (!qtitem)
-        return;
+void sendQtKeyEvent(Item *item, bool press, int keyCode,
+                    QtEvents::KeyModifier mod) {
+  auto qtitem = dynamic_cast<QtItem *>(item);
+  if (!qtitem)
+    return;
 
-    auto window = qtitem->qquickitem()->window();
+  auto window = qtitem->qquickitem()->window();
 
-    auto qtmod = getQtKeyboardModifiers(mod);
-    auto keyEvent = new QKeyEvent(press ? QEvent::KeyPress : QEvent::KeyRelease, keyCode, qtmod);
-    QGuiApplication::postEvent(window, keyEvent);
+  auto qtmod = getQtKeyboardModifiers(mod);
+  auto keyEvent = new QKeyEvent(press ? QEvent::KeyPress : QEvent::KeyRelease,
+                                keyCode, qtmod);
+  QGuiApplication::postEvent(window, keyEvent);
 }
 
 } // namespace
 
-void QtEvents::mouseDown(Item* item, Point loc, MouseButton button)
-{
-    QPointF windowLoc;
-    auto window = getWindowAndPositionForItem(item, loc, windowLoc);
-    if (!window)
-        return;
+void QtEvents::mouseDown(Item *item, Point loc, MouseButton button) {
+  QPointF windowLoc;
+  auto window = getWindowAndPositionForItem(item, loc, windowLoc);
+  if (!window)
+    return;
 
-    m_pressedMouseButtons |= button;
-    Qt::MouseButton eventCausingButton = getQtMouseButtonValue(button);
-    Qt::MouseButtons activeButtons = getQtMouseButtonValue(m_pressedMouseButtons);
+  m_pressedMouseButtons |= button;
+  Qt::MouseButton eventCausingButton = getQtMouseButtonValue(button);
+  Qt::MouseButtons activeButtons = getQtMouseButtonValue(m_pressedMouseButtons);
 
-    QMouseEvent* event = new QMouseEvent(QEvent::MouseButtonPress, windowLoc, eventCausingButton, activeButtons, Qt::KeyboardModifier::NoModifier);
-    QGuiApplication::postEvent(window, event);
+  QMouseEvent *event =
+      new QMouseEvent(QEvent::MouseButtonPress, windowLoc, eventCausingButton,
+                      activeButtons, Qt::KeyboardModifier::NoModifier);
+  QGuiApplication::postEvent(window, event);
 }
 
-void QtEvents::mouseUp(Item* item, Point loc, MouseButton button)
-{
-    QPointF windowLoc;
-    auto window = getWindowAndPositionForItem(item, loc, windowLoc);
-    if (!window)
-        return;
+void QtEvents::mouseUp(Item *item, Point loc, MouseButton button) {
+  QPointF windowLoc;
+  auto window = getWindowAndPositionForItem(item, loc, windowLoc);
+  if (!window)
+    return;
 
-    m_pressedMouseButtons ^= button;
-    Qt::MouseButton eventCausingButton = getQtMouseButtonValue(button);
-    Qt::MouseButtons activeButtons = getQtMouseButtonValue(m_pressedMouseButtons);
+  m_pressedMouseButtons ^= button;
+  Qt::MouseButton eventCausingButton = getQtMouseButtonValue(button);
+  Qt::MouseButtons activeButtons = getQtMouseButtonValue(m_pressedMouseButtons);
 
-    QMouseEvent* event = new QMouseEvent(QEvent::MouseButtonRelease, windowLoc, eventCausingButton, activeButtons, Qt::KeyboardModifier::NoModifier);
-    QGuiApplication::postEvent(window, event);
+  QMouseEvent *event =
+      new QMouseEvent(QEvent::MouseButtonRelease, windowLoc, eventCausingButton,
+                      activeButtons, Qt::KeyboardModifier::NoModifier);
+  QGuiApplication::postEvent(window, event);
 }
 
-void QtEvents::mouseMove(Item* item, Point loc)
-{
-    QPointF windowLoc;
-    auto window = getWindowAndPositionForItem(item, loc, windowLoc);
-    if (!window)
-        return;
+void QtEvents::mouseMove(Item *item, Point loc) {
+  QPointF windowLoc;
+  auto window = getWindowAndPositionForItem(item, loc, windowLoc);
+  if (!window)
+    return;
 
-    Qt::MouseButton activeButtons = getQtMouseButtonValue(m_pressedMouseButtons);
+  Qt::MouseButton activeButtons = getQtMouseButtonValue(m_pressedMouseButtons);
 
-    // Wiggle the cursor a bit. This is needed to correctly recognize drag events
-    windowLoc.rx() -= 1;
-    QMouseEvent* mouseMoveEvent
-        = new QMouseEvent(QEvent::MouseMove, windowLoc, Qt::MouseButton::NoButton, activeButtons, Qt::KeyboardModifier::NoModifier);
-    QGuiApplication::postEvent(window, mouseMoveEvent);
+  // Wiggle the cursor a bit. This is needed to correctly recognize drag events
+  windowLoc.rx() -= 1;
+  QMouseEvent *mouseMoveEvent =
+      new QMouseEvent(QEvent::MouseMove, windowLoc, Qt::MouseButton::NoButton,
+                      activeButtons, Qt::KeyboardModifier::NoModifier);
+  QGuiApplication::postEvent(window, mouseMoveEvent);
 
-    // Wiggle the cursor a bit. This is needed to correctly recognize drag events
-    windowLoc.rx() += 1;
-    mouseMoveEvent = new QMouseEvent(QEvent::MouseMove, windowLoc, Qt::MouseButton::NoButton, activeButtons, Qt::KeyboardModifier::NoModifier);
-    QGuiApplication::postEvent(window, mouseMoveEvent);
+  // Wiggle the cursor a bit. This is needed to correctly recognize drag events
+  windowLoc.rx() += 1;
+  mouseMoveEvent =
+      new QMouseEvent(QEvent::MouseMove, windowLoc, Qt::MouseButton::NoButton,
+                      activeButtons, Qt::KeyboardModifier::NoModifier);
+  QGuiApplication::postEvent(window, mouseMoveEvent);
 }
 
-void QtEvents::stringInput(Item* item, const std::string& text)
-{
-    auto qtitem = dynamic_cast<QtItem*>(item);
-    if (!qtitem)
-        return;
+void QtEvents::stringInput(Item *item, const std::string &text) {
+  auto qtitem = dynamic_cast<QtItem *>(item);
+  if (!qtitem)
+    return;
 
-    auto window = qtitem->qquickitem()->window();
+  auto window = qtitem->qquickitem()->window();
 
-    auto keyDownEvent
-        = new QKeyEvent(QEvent::KeyPress, 0 /* key id */, Qt::KeyboardModifier::NoModifier, QString::fromStdString(text));
-    QGuiApplication::postEvent(window, keyDownEvent);
+  auto keyDownEvent = new QKeyEvent(QEvent::KeyPress, 0 /* key id */,
+                                    Qt::KeyboardModifier::NoModifier,
+                                    QString::fromStdString(text));
+  QGuiApplication::postEvent(window, keyDownEvent);
 }
 
-void QtEvents::keyPress(Item* item, int keyCode, KeyModifier mod)
-{
-    sendQtKeyEvent(item, true, keyCode, mod);
+void QtEvents::keyPress(Item *item, int keyCode, KeyModifier mod) {
+  sendQtKeyEvent(item, true, keyCode, mod);
 }
 
-void QtEvents::keyRelease(Item* item, int keyCode, KeyModifier mod)
-{
-    sendQtKeyEvent(item, false, keyCode, mod);
+void QtEvents::keyRelease(Item *item, int keyCode, KeyModifier mod) {
+  sendQtKeyEvent(item, false, keyCode, mod);
 }
 
-void QtEvents::extMouseDrop(Item* item, Point loc, PasteboardContent& content)
-{
-    auto qtitem = dynamic_cast<QtItem*>(item);
-    if (!qtitem)
-        return;
+void QtEvents::extMouseDrop(Item *item, Point loc, PasteboardContent &content) {
+  auto qtitem = dynamic_cast<QtItem *>(item);
+  if (!qtitem)
+    return;
 
-    auto window = qtitem->qquickitem()->window();
+  auto window = qtitem->qquickitem()->window();
 
-    QPointF qtlocalPoint(loc.x, loc.y);
-    QPointF windowLoc = qtitem->qquickitem()->mapToScene(qtlocalPoint);
-    QPoint windowLocInt(windowLoc.rx(), windowLoc.ry());
+  QPointF qtlocalPoint(loc.x, loc.y);
+  QPointF windowLoc = qtitem->qquickitem()->mapToScene(qtlocalPoint);
+  QPoint windowLocInt(windowLoc.rx(), windowLoc.ry());
 
-    auto mimeData = new QMimeData();
-    QList<QUrl> urlList;
-    for (const auto& url : content.urls()) {
-        QUrl qurl(QString::fromStdString(url));
-        urlList.append(qurl);
-    }
-    mimeData->setUrls(urlList);
+  auto mimeData = new QMimeData();
+  QList<QUrl> urlList;
+  for (const auto &url : content.urls()) {
+    QUrl qurl(QString::fromStdString(url));
+    urlList.append(qurl);
+  }
+  mimeData->setUrls(urlList);
 
-    auto enter = new QDragEnterEvent(
-        windowLocInt, Qt::CopyAction | Qt::MoveAction | Qt::LinkAction, mimeData, Qt::MouseButton::NoButton, Qt::KeyboardModifier::NoModifier);
-    QGuiApplication::postEvent(window, enter);
+  auto enter = new QDragEnterEvent(
+      windowLocInt, Qt::CopyAction | Qt::MoveAction | Qt::LinkAction, mimeData,
+      Qt::MouseButton::NoButton, Qt::KeyboardModifier::NoModifier);
+  QGuiApplication::postEvent(window, enter);
 
-    auto move = new QDragMoveEvent(
-        windowLocInt, Qt::CopyAction | Qt::MoveAction | Qt::LinkAction, mimeData, Qt::MouseButton::NoButton, Qt::KeyboardModifier::NoModifier);
-    QGuiApplication::postEvent(window, move);
+  auto move = new QDragMoveEvent(
+      windowLocInt, Qt::CopyAction | Qt::MoveAction | Qt::LinkAction, mimeData,
+      Qt::MouseButton::NoButton, Qt::KeyboardModifier::NoModifier);
+  QGuiApplication::postEvent(window, move);
 
-    auto drop = new QDropEvent(
-        windowLoc, Qt::CopyAction | Qt::MoveAction | Qt::LinkAction, mimeData, Qt::MouseButton::NoButton, Qt::KeyboardModifier::NoModifier);
-    QGuiApplication::postEvent(window, drop);
+  auto drop = new QDropEvent(
+      windowLoc, Qt::CopyAction | Qt::MoveAction | Qt::LinkAction, mimeData,
+      Qt::MouseButton::NoButton, Qt::KeyboardModifier::NoModifier);
+  QGuiApplication::postEvent(window, drop);
 }
 
-void QtEvents::quit()
-{
-    QGuiApplication::quit();
-}
+void QtEvents::quit() { QGuiApplication::quit(); }
 
 } // namespace spix
