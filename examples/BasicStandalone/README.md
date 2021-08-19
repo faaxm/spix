c724a9aab76598f520bbe771b7221add19b9d3e5
Falko Axmann <code@falkoaxmann.de>
2021-08-19 18:02:24 +0200

    Include basic standalone example
    The standalone example can be built outside of the Spix
    project. It serves as a template for other projects and
    as a CI test that the install of Spix worked.

---
+++ examples/BasicStandalone/README.md
@@ -0,0 +1,12 @@
+# Example: BasicStandalone
+
+This example has the same code as the 'Basic' example, but is designed as a standalone project. You can use it as a template to use Spix in your own project.
+Since this example is a standalone project, it is not built as part of the "Spix" project.
+
+After installing Spix, you can build this example like this:
+```shell
+cd examples/BasicStandalone
+mkdir build
+cmake -B build .
+cmake --build build
+```
---
+++ examples/BasicStandalone/main.cpp
@@ -0,0 +1,50 @@
+/***
+ * Copyright (C) Falko Axmann. All rights reserved.
+ * Licensed under the MIT license.
+ * See LICENSE.txt file in the project root for full license information.
+ ****/
+
+#include <QGuiApplication>
+#include <QQmlApplicationEngine>
+#include <Spix/QtQmlBot.h>
+
+#include <iostream>
+
+class MyTests : public spix::TestServer {
+protected:
+    void executeTest() override
+    {
+        mouseClick(spix::ItemPath("mainWindow/Button_1"));
+        wait(std::chrono::milliseconds(500));
+        mouseClick(spix::ItemPath("mainWindow/Button_2"));
+        wait(std::chrono::milliseconds(500));
+        mouseClick(spix::ItemPath("mainWindow/Button_2"));
+        wait(std::chrono::milliseconds(500));
+        mouseClick(spix::ItemPath("mainWindow/Button_1"));
+        wait(std::chrono::milliseconds(500));
+        mouseClick(spix::ItemPath("mainWindow/Button_2"));
+        wait(std::chrono::milliseconds(500));
+        mouseClick(spix::ItemPath("mainWindow/Button_1"));
+        wait(std::chrono::milliseconds(500));
+
+        auto result = getStringProperty("mainWindow/results", "text");
+        std::cout << "-------\nResult:\n-------\n" << result << "\n-------" << std::endl;
+    }
+};
+
+int main(int argc, char* argv[])
+{
+    // Init Qt Qml Application
+    QGuiApplication app(argc, argv);
+    QQmlApplicationEngine engine;
+    engine.load(QUrl(QStringLiteral("qrc:/main.qml")));
+    if (engine.rootObjects().isEmpty())
+        return -1;
+
+    // Instantiate and run tests
+    MyTests tests;
+    auto bot = new spix::QtQmlBot();
+    bot->runTestServer(tests);
+
+    return app.exec();
+}
