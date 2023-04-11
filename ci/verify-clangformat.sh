1fd7b74d02ab2b40dc09ecb51efeecaebc09389a
Falko Axmann <code@falkoaxmann.de>
2023-04-11 23:51:44 +0200

    fix clang-format
    use a github-action to check formatting using a fixed
    clang-format version (v11).

--- ci/verify-clangformat.sh
+++ ci/verify-clangformat.sh
@@ -1,11 +1,22 @@
+
+clang-format --version
 sh devtools/run-clang-format.sh
 
 if git diff-index --quiet HEAD --
 then
     echo "Success: Clang-format ran without changing the code.";
 else
+    echo;
+    echo;
+    echo "################################################################################";
     echo "Error: Please run clang-format (e.g. via devtools/run-clang-format.sh)";
-    echo "Running clang-format on the buildserver introduced changes in the source which indicates it has not been run on the last commit";
+    echo;
+    echo "Running clang-format on the buildserver introduced changes in the source";
+    echo "which indicates it has not been run on the last commit. Please fix the formatting";
+    echo "by running clang-format locally and committing the changes.";
+    echo "################################################################################";
+    echo;
+    echo;
     
     git diff;
     
