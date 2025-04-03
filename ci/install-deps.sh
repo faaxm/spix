2d6edd0fc83a6db88cfe52dc33275e28c1819777
Falko Axmann <51922941+faaxm@users.noreply.github.com>
2025-04-03 11:31:20 +0200

    Fix CI Builds (#123)
    * Make sure that the install-deps.sh script propagates errors to the script caller.
    * override cmake version num when configuring anyrpc
    
    The original anyrpc cmake file specifies a very old min cmake version which
    is no longer supported by modern cmake.

--- ci/install-deps.sh
+++ ci/install-deps.sh
@@ -1,15 +1,18 @@
 #!/bin/sh
 
+# Exit on error
+set -e
+
 # compile & install
 mkdir deps && cd deps
 # -> anyrpc
 git clone --recursive https://github.com/sgieseking/anyrpc.git
 cd anyrpc && mkdir build && cd build
 if [[ $RUNNER_OS == "Windows" ]]; then
-    cmake -G"Visual Studio 17 2022" -DCMAKE_BUILD_TYPE=$CI_BUILD_TYPE -DBUILD_EXAMPLES=OFF -DBUILD_WITH_LOG4CPLUS=OFF -DANYRPC_LIB_BUILD_SHARED=${SHARED_LIBS} ..
+    cmake -G"Visual Studio 17 2022" -DCMAKE_POLICY_VERSION_MINIMUM=3.5 -DCMAKE_BUILD_TYPE=$CI_BUILD_TYPE -DBUILD_EXAMPLES=OFF -DBUILD_WITH_LOG4CPLUS=OFF -DANYRPC_LIB_BUILD_SHARED=${SHARED_LIBS} ..
     cmake --build . --target install --config $CI_BUILD_TYPE
 else
-    cmake -G"Unix Makefiles" -DCMAKE_BUILD_TYPE=$CI_BUILD_TYPE -DBUILD_EXAMPLES=OFF -DBUILD_WITH_LOG4CPLUS=OFF -DANYRPC_LIB_BUILD_SHARED=${SHARED_LIBS} ..
+    cmake -G"Unix Makefiles" -DCMAKE_POLICY_VERSION_MINIMUM=3.5 -DCMAKE_BUILD_TYPE=$CI_BUILD_TYPE -DBUILD_EXAMPLES=OFF -DBUILD_WITH_LOG4CPLUS=OFF -DANYRPC_LIB_BUILD_SHARED=${SHARED_LIBS} ..
     sudo cmake --build . --target install --config $CI_BUILD_TYPE
 fi
 # leave anyrpc
